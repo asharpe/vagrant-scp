8206550801f0fb44206c34d5e6f64a0976c855a1
W. Andrew Loe III <andrew@andrewloe.com>
2013-06-21 14:51:32 -0700

    bundle gem vagrant-puppet-scp

---
+++ lib/vagrant/puppet/scp.rb
@@ -0,0 +1,9 @@
+require "vagrant/puppet/scp/version"
+
+module Vagrant
+  module Puppet
+    module Scp
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/vagrant/puppet/scp/version.rb
@@ -0,0 +1,7 @@
+module Vagrant
+  module Puppet
+    module Scp
+      VERSION = "0.0.1"
+    end
+  end
+end
